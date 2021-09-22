266ad64ad21ceee9f22a80f594484b6e3d008946
rw1n <arwingoo90@gmail.com>
2021-09-22 23:38:53 +0800

    Added hover effect for left-navbar list items

--- landing.js
+++ landing.js
@@ -2,8 +2,8 @@
 // console.log(leftNavElements.length);
 let leftNavElements = (document.querySelectorAll("#left-nav-elements li"));
 
+// add an on-click event listener to each li item in left navbar
 for (let i = 0; i < leftNavElements.length; i++) {
-    
     leftNavElements[i].addEventListener("click", function (){
         document.querySelector(".selected").classList.remove("selected");
         leftNavElements[i].classList.add("selected");
